677a998b0884036b3d84d4d51b94c86d63953ba5
vader1359 <vader1359@gmail.com>
2017-06-23 20:53:59 +0700

    Pure project

---
+++ Dep/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dep
+//
+//  Created by Vader1359 on 6/23/17.
+//  Copyright © 2017 Vader1359. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Dep' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Dep
+  pod 'AFNetworking', '~> 3.0'
+  pod 'MarqueeLabel'
+
+end
